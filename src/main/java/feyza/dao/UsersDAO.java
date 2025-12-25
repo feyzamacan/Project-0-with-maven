89ac4af3ae5a8c4cc2f098ed735451759f26238b
feyza <feyzamacan@gmail.com>
2025-12-25 16:58:54 -0600

    Initial commit

---
+++ src/main/java/feyza/dao/UsersDAO.java
@@ -0,0 +1,15 @@
+package feyza.dao;
+import java.sql.*;
+
+import com.feyza.models.User;
+
+public interface UsersDao {
+  public User getUser(Connection con, String username) throws SQLException;
+  public int insertUser(Connection con, User u) throws SQLException;
+  public int updateUser(Connection con, User u) throws SQLException;
+  public int deleteUser(Connection con, User u) throws SQLException;
+  public int updateUser(Connection con, User u, float balance) throws SQLException;
+}
+
+//the interface defines the rules for how user data is accessed in the database
+//without implementing the actual SQL.
